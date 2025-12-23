7c9516991bf18d5a1457c8c7ed6a77e7355a0fd5
NullPointer <vanshbhura453@gmail.com>
2025-12-23 16:12:45 +0530

    Create day01_basics.py

---
+++ day01_basics.py
@@ -0,0 +1,27 @@
+# Day 1: Python Basics
+# Topics covered:
+# - Variables
+# - Input and Output
+# - Type conversion
+# - Basic arithmetic
+
+# Taking user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Displaying output
+print("Hello", name)
+print("You are", age, "years old")
+
+# Arithmetic operation
+next_year_age = age + 1
+print("Next year, you will be", next_year_age, "years old")
+
+# Simple calculations
+num1 = 10
+num2 = 3
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
