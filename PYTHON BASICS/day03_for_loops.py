63aa83f527aa66c94b44f7827c942ce5d57736a2
NullPointer <vanshbhura453@gmail.com>
2025-12-24 10:54:49 +0530

    day03_for_loops.py

---
+++ PYTHON BASICS/day03_for_loops.py
@@ -0,0 +1,16 @@
+# Day 3: For Loops
+# Topics:
+# for loop, range(), repetition
+
+print("Numbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
+
+print("\nEven numbers from 1 to 10:")
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+
+print("\nMultiplication table of 5:")
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
