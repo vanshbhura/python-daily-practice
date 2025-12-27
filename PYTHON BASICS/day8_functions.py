f49836a2f5e63d809ef847fed226f12c7f5bdd8e
NullPointer <vanshbhura453@gmail.com>
2025-12-27 11:03:47 +0530

    day8_functions.py

---
+++ PYTHON BASICS/day8_functions.py
@@ -0,0 +1,21 @@
+# Day 8: Functions
+# Topics:
+# def, parameters, return
+
+def greet(name):
+    print("Hello", name)
+
+def add(a, b):
+    return a + b
+
+def square(number):
+    return number * number
+
+# Function calls
+greet("Vansh")
+
+result = add(10, 5)
+print("Sum:", result)
+
+sq = square(4)
+print("Square:", sq)
