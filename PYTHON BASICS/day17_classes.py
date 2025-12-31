7f2fcbd011680b885f19026f12721cc60fba5abb
NullPointer <vanshbhura453@gmail.com>
2025-12-31 18:15:51 +0530

    day17_classes.py

---
+++ PYTHON BASICS/day17_classes.py
@@ -0,0 +1,14 @@
+# Day 17: Classes and Objects
+# Topics:
+# class, object, attributes, methods
+
+class Student:
+    def display_info(self):
+        print("This is a student object")
+
+# Creating objects
+student1 = Student()
+student2 = Student()
+
+student1.display_info()
+student2.display_info()
