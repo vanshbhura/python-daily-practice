7e9a367d743d821b3d4c0244b7574e067793501d
NullPointer <vanshbhura453@gmail.com>
2025-12-31 18:06:39 +0530

    Create math_utils.py

---
+++ PYTHON BASICS/math_utils.py
@@ -0,0 +1,11 @@
+# math_utils.py
+# This module contains basic math utility functions
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
