f5a84b511551b4d23e725c0958da53ebf6733c71
NullPointer <vanshbhura453@gmail.com>
2025-12-31 18:08:11 +0530

    day16_modules.py

---
+++ PYTHON BASICS/day16_modules.py
@@ -0,0 +1,12 @@
+# Day 16: Modules and Import
+# Topics:
+# creating modules, importing functions
+
+import math_utils
+
+x = 10
+y = 5
+
+print("Addition:", math_utils.add(x, y))
+print("Subtraction:", math_utils.subtract(x, y))
+print("Multiplication:", math_utils.multiply(x, y))
